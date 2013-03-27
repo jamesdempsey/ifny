f233eca8c02a828d4e681d8869563c3cfc0fb6a1
James Dempsey <james.e.dempsey@gmail.com>
2013-03-27 13:21:57 -0400

    added resource routes for films and theaters

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Ifny::Application.routes.draw do
+  root to: 'films#index'
+
+  resources :films,    only: [:index, :show]
+  resources :theaters, only: [:index, :show]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
