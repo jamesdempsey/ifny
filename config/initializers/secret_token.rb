2ddb3ef869f8709b987f1b4e3eee5bad6e02acaf
James Dempsey <james.e.dempsey@gmail.com>
2013-03-27 03:31:07 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ifny::Application.config.secret_key_base = '908e7f47d724326fbefc7709a48d462598209d1a2ac71114d6df062bb0700f9d8ede736630e66fb2d0980b4723fb4ca70864633a6f3553f7c2f1701073b6de73'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ifny::Application.config.session_store :encrypted_cookie_store, key: '_ifny_session'
