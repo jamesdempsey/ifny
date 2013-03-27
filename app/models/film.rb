d4fa31b257d2658098f9c9d0c28e4a00e89a94a9
James Dempsey <james.e.dempsey@gmail.com>
2013-03-27 12:58:12 -0400

    conditions on model relationships deprecated in rails 4 in favor of required scope blocks

--- app/models/film.rb
+++ app/models/film.rb
@@ -1,6 +1,6 @@
 class Film < ActiveRecord::Base
   validates :url, uniqueness: true
 
-  has_many :showings, order: 'showtime ASC'
-  has_many :theaters, through: :showings, uniq: true
+  has_many :showings, -> { order 'showtime ASC' }
+  has_many :theaters, -> { uniq true }, through: :showings
 end
--- app/models/theater.rb
+++ app/models/theater.rb
@@ -2,5 +2,5 @@ class Theater < ActiveRecord::Base
   validates :url, uniqueness: true
 
   has_many :showings
-  has_many :films, through: :showings, uniq: true
+  has_many :films, -> { uniq true }, through: :showings
 end
