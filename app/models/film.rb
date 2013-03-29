0717f598acc7d572d39837098ea7eb73c43fd4e8
James Dempsey <james.e.dempsey@gmail.com>
2013-03-29 00:24:55 -0400

    setup film poster image uploader

--- app/models/film.rb
+++ app/models/film.rb
@@ -3,4 +3,6 @@ class Film < ActiveRecord::Base
 
   has_many :showings, -> { order 'showtime ASC' }
   has_many :theaters, -> { uniq true }, through: :showings
+
+  mount_uploader :poster, PosterUploader
 end
