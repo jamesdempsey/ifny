b07ef10a9b987a018a81d91153f3b6a3afd411de
James Dempsey <james.e.dempsey@gmail.com>
2013-03-29 17:44:45 -0400

    expand item containers to image poster size

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -9,20 +9,29 @@ $(function() {
 
   $('.item').click(function() {
     var $this = $(this),
-        tileStyle = $this.hasClass('expanded') ? { width: 176, height: 176 } : { width: 350, height: 350 };
+        tileStyle = $this.hasClass('expanded') ?
+          { width: 176, height: $this.height() / 2 } : { width: 352, height: $this.height() * 2 };
     $this.toggleClass('expanded');
     $this.find('.item-content').animate(tileStyle);
     $container.isotope('reLayout');
   });
+
+  $('.item img').click(function() {
+    $(this).animate({
+      top: '-=' + $(this).height() / 2,
+      left: '-=' + $(this).width() / 2,
+      width: $(this).width() * 2
+    });
+  });
 });
 
 // Randomly color item divs
-$(function() {
-  $('.item-content').each(function() {
-    var randRGB = function() {
-      return Math.floor((256-199)*Math.random()) + 200;
-    },
-    hue = 'rgb(' + [randRGB(), randRGB(), randRGB()].join(',') + ')';
-    $(this).css('background-color', hue);
-  });
-});
+//$(function() {
+  //$('.item-content').each(function() {
+    //var randRGB = function() {
+      //return Math.floor((256-199)*Math.random()) + 200;
+    //},
+    //hue = 'rgb(' + [randRGB(), randRGB(), randRGB()].join(',') + ')';
+    //$(this).css('background-color', hue);
+  //});
+//});
