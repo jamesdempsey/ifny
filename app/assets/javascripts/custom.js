d9d31a0c95cc1546d67efbf172bcfd7315e5afce
James Dempsey <james.e.dempsey@gmail.com>
2013-03-28 22:37:20 -0400

    randomly color item divs for testing

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -3,5 +3,14 @@ $(function() {
     masonry: {
       columnWidth: 240
     }
+
+// Randomly color item divs
+$(function() {
+  $('.item-content').each(function() {
+    var randRGB = function() {
+      return Math.floor((256-199)*Math.random()) + 200;
+    },
+    hue = 'rgb(' + [randRGB(), randRGB(), randRGB()].join(',') + ')';
+    $(this).css('background-color', hue);
   });
 });
