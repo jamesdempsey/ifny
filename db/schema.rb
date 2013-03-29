721c7e53aa2c6cd2ab61a7ee280f490e9c8fc4e2
James Dempsey <james.e.dempsey@gmail.com>
2013-03-29 00:25:18 -0400

    migrated for film poster image uploader

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130327080453) do
+ActiveRecord::Schema.define(version: 20130329024919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20130327080453) do
     t.string   "url"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "poster"
   end
 
   add_index "films", ["url"], name: "index_films_on_url", unique: true
