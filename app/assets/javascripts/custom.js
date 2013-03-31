5b7fd6c9663057a69c837316be9e507a919e6a09
James Dempsey <james.e.dempsey@gmail.com>
2013-03-31 03:01:12 -0400

    resized layout elements, cleaned up and refactored

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -10,23 +10,27 @@ $(function() {
 
   $item.click(function() {
     var $this = $(this), $img = $this.find('img'),
+        $desc = $this.find('.item-description'),
         itemStyle = $this.hasClass('expanded') ?
           { width: 176, height: $this.height() / 2 } :
-          { width: 528, height: $this.height() * 2 },
+          { width: 704, height: $this.height() * 2 },
         imgStyle = $this.hasClass('expanded') ?
           { top: '+=' + $this.height() * 2,
             left: '+=' + $this.width() * 2,
             width: $this.width() / 2 } :
           { top: '-=' + $this.height() / 2,
             left: '-=' + $this.width() / 2,
-            width: $this.width() * 2 };
+            width: $this.width() * 2 },
+        descStyle = $this.hasClass('expanded') ?
+          { opacity: 0 } : { opacity: 1 },
         scrollTo = function() {
           $('html, body').animate({
             scrollTop: $this.offset().top - 40
-          });
+          }, 200, 'linear');
         };
 
     $this.toggleClass('expanded');
+    $desc.animate(descStyle);
     $this.css(itemStyle);
     $img.animate(imgStyle);
     $this.find('.item-content').stop().animate(itemStyle, scrollTo);
