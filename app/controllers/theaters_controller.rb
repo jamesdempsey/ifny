4b55cb38c12b951f58fdf7d3c27ebb4fc5819247
James Dempsey <james.e.dempsey@gmail.com>
2013-04-01 03:03:56 -0400

    render theater/scoped showtimes json for links in item containers

--- app/controllers/theaters_controller.rb
+++ app/controllers/theaters_controller.rb
@@ -2,4 +2,10 @@ class TheatersController < ApplicationController
   def index
     @theaters = Theater.all
   end
+
+  def show
+    @theater = Theater.find(params[:id])
+    @showtimes = @theater.film_showtimes(params[:film_id].to_i)
+    render json: {theater: @theater, showtimes: @showtimes}
+  end
 end
