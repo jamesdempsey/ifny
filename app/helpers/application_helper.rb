54dc6682d2d7cb3b950ab4f9caa5f62e9b9b38b2
James Dempsey <james.e.dempsey@gmail.com>
2013-04-02 15:42:35 -0400

    changed helper method and image tag for poster images mounted on image model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def container_dimensions(image)
-    width = image.get_dimensions[:width] / 2
-    height = image.get_dimensions[:height] / 2
+    width = image.width / 2
+    height = image.height / 2
     ['width: ', width, 'px; height: ', height, 'px'].join
   end
 end
