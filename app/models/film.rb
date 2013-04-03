20d01d91c585f7ba68e4fc11662fd431b8c527f3
James Dempsey <james.e.dempsey@gmail.com>
2013-04-03 13:29:08 -0400

    added scope to return only films with at least one active showing

--- app/models/film.rb
+++ app/models/film.rb
@@ -4,4 +4,6 @@ class Film < ActiveRecord::Base
   has_many :showings, -> { order 'showtime ASC' }
   has_many :theaters, -> { uniq true }, through: :showings
   has_many :images
+
+  scope :active, -> { joins(:showings).group('films.id').merge(Showing.active) }
 end
