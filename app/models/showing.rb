789296e4423455f7e15391dc7f21e3c0152b17fb
James Dempsey <james.e.dempsey@gmail.com>
2013-04-04 19:24:46 -0400

    moved showtime stringify method to showing model

--- app/models/showing.rb
+++ app/models/showing.rb
@@ -4,4 +4,8 @@ class Showing < ActiveRecord::Base
 
   scope :by_theater, lambda { |theater| where(theater_id: theater.id) }
   scope :active, -> { where('showtime >= ?', Time.now - 1.day) }
+
+  def showtime_string
+    [showtime.strftime('%a, %b %-d at'), showtime.strftime('%l:%M%P').strip].join(' ')
+  end
 end
