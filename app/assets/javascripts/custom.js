3cbed0b30a7f1f97d9e6bdf41e74b75f3f409fda
James Dempsey <james.e.dempsey@gmail.com>
2013-04-04 19:30:51 -0400

    js overhaul to grab json on page load, use it for showtimes in item containers

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,5 +1,7 @@
 $(function() {
-  var $container = $('#container'), $hidden = $('#hidden'), $item = $('.item');
+  var $container = $('#container'),
+    $hidden = $('#hidden'),
+    $item = $('.item');
 
   $container.isotope({
     itemSelector: '.item',
@@ -7,6 +9,56 @@ $(function() {
       columnWidth: 186
     }
   }).isotope('insert', $hidden.find('.item'));
+
+  $('a.theater-name').click(function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+  });
+
+  (function() {
+    $.getJSON('/films', function(json) {
+      $('a.theater-name').click(function(e) {
+        var $this = $(this),
+          $film_showtimes_div = $this.closest('.film-showtimes'),
+          $film_showtimes_index_div = $this.closest('.film-showtimes-index'),
+          $film_id = +$this.closest('.item').attr('id'),
+          $theater_id = +$this.attr('href').match(/\d+/),
+          $film = $.grep(json['films'], function(film) {
+            return film.id == $film_id;
+          })[0],
+          $film_showtimes = $.map($film.showings, function(showing, index) {
+            return showing.showtime;
+          }),
+          $showtimes_theater = [
+            '<div class="film-showtimes-theater">',
+            '<div class="title">', $this.text(), '</div>',
+            '<ul><li>',
+            $film_showtimes.join('</li><li>'),
+            '</li></ul>',
+            '<a href="#" class="back">Back</a></div>'
+          ].join('');
+
+        $film_showtimes_index_div.animate({ opacity: 0}, function() {
+          $film_showtimes_div.append($showtimes_theater);
+          $('a.back').click(function(e) {
+            var $this = $(this),
+              $film_showtimes_div = $this.closest('.film-showtimes'),
+              $theater = $film_showtimes_div.find('.film-showtimes-theater'),
+              $film_showtimes_index_div = $film_showtimes_div.find('.film-showtimes-index');
+
+            e.stopPropagation();
+            e.preventDefault();
+            $theater.animate({ opacity: 0}, function() {
+              $film_showtimes_index_div.animate({ opacity: 1});
+              $(this).remove();
+            });
+          });
+
+          $film_showtimes_div.animate({ opacity: 1}, 'slow');
+        });
+      });
+    })
+  })();
 
   $item.click(function() {
     var $this = $(this), $img = $this.find('img'),
@@ -39,48 +91,5 @@ $(function() {
     $img.animate(imgStyle);
     $this.find('.item-content').stop().animate(itemStyle, scrollTo);
     $container.isotope('reLayout', scrollTo);
-  });
-
-  // WAYYYYY EXPERIMENTALLLLLL
-  //
-  // HERE WE GO
-  //
-  $('a.theater-name').click(function(e) {
-    var $this = $(this), $film_showtimes = $this.closest('.film-showtimes'),
-      $index = $this.closest('.film-showtimes-index');
-    e.stopPropagation();
-    e.preventDefault();
-    $.getJSON($this.attr('href'), {film_id: $this.closest('.item').attr('id')},
-      function(json) {
-        var $showtimes = $.map(json.showtimes, function(showtime, index) {
-          return showtime;
-        }), $theater = json.theater['theater'].name,
-        $showtimes_theater = [
-          '<div class="film-showtimes-theater">',
-          '<div class="title">', $theater, '</div>',
-          '<ul><li>',
-          $showtimes.join('</li><li>'),
-          '</li></ul>',
-          '<a href="#" class="back">Back</a></div>'
-        ].join('');
-
-        $index.animate({ opacity: 0}, function() {
-          $film_showtimes.append($showtimes_theater);
-          $('a.back').click(function(e) {
-            var $this = $(this), $film_showtimes = $this.closest('.film-showtimes'),
-              $theater = $film_showtimes.find('.film-showtimes-theater'),
-              $index = $film_showtimes.find('.film-showtimes-index');
-
-            e.stopPropagation();
-            e.preventDefault();
-            $theater.animate({ opacity: 0}, function() {
-              $index.animate({ opacity: 1});
-              $(this).remove();
-            });
-          });
-          $film_showtimes.animate({ opacity: 1});
-        });
-      }
-    );
   });
 });
