bdb51c5fe7457c1c609ad1736079c7259ace4058
James Dempsey <james.e.dempsey@gmail.com>
2013-04-06 16:24:49 -0400

    generate class methods for theater url lookup, concat

--- app/models/theater.rb
+++ app/models/theater.rb
@@ -6,4 +6,10 @@ class Theater < ActiveRecord::Base
 
   URLS = {ifc: 'http://www.ifccenter.com/',
           angelika: 'http://www.angelikafilmcenter.com/'}
+
+  URLS.keys.each do |url|
+    define_singleton_method("#{url}_url".to_s) do |*url_strings|
+      [URLS[url], *url_strings].join
+    end
+  end
 end
